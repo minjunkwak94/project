17ff93ca834a79c6d84379cbb1292e84293cabfa
linibell <linibell@naver.com>
2020-11-19 17:02:56 +0900

    community update and accounts

--- community/serializers.py
+++ community/serializers.py
@@ -1,16 +1,46 @@
 from rest_framework import serializers
 from .models import Post, Comment
 
-class PostSerializer(serializers.ModelSerializer):
-  
-    class Meta:
-        model = Post
-        fields = ('id', 'category', 'title', 'content', 'created_at', 'updated_at')
+# class CommentSerializer(serializers.ModelSerializer):
 
-class CommentSerializer(serializers.ModelSerializer):
-    # post = serializers.IntegerField(read_only=True)
+#     class Meta:
+#         model = Comment
+#         fields = ('id', 'post', 'content', 'created_at', 'updated_at')
+#         read_only_fields = ('post',)
+
+# class PostSerializer(serializers.ModelSerializer):
+
+#     comment_set = CommentSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = Post
+#         fields = ('id', 'category', 'title', 'content', 'created_at', 'updated_at', 'comment_set')
+
+
+class CommentListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'content',)
+        read_only_fields = ('post',)
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
         fields = ('id', 'post', 'content', 'created_at', 'updated_at')
-        read_only_fields = ('post',)
+
+
+class PostListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ('id', 'category', 'title', )
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+
+    comment_set = CommentListSerializer(many=True, read_only=True)
+    class Meta:
+        model = Post
+        fields = ('id', 'category', 'title', 'content', 'created_at', 'updated_at', 'comment_set')
