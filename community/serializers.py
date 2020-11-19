14cc8ca3613c238509f8283bd6c4f6174759abe4
linibell <linibell@naver.com>
2020-11-19 15:06:38 +0900

    community_comment

--- community/serializers.py
+++ community/serializers.py
@@ -3,12 +3,14 @@ from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
   
-  class Meta:
-    model = Post
-    fields = ('id', 'category', 'title', 'content', 'created_at', 'updated_at')
+    class Meta:
+        model = Post
+        fields = ('id', 'category', 'title', 'content', 'created_at', 'updated_at')
 
 class CommentSerializer(serializers.ModelSerializer):
-  
-  class Meta:
-    model = Comment
-    fields = ('id', 'post', 'content', 'created_at', 'updated_at')
+    # post = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'content', 'created_at', 'updated_at')
+        read_only_fields = ('post',)
--- community/views.py
+++ community/views.py
@@ -31,3 +31,23 @@ def post_detail_update_delete(request, post_pk):
         post.delete()
         return Response({ 'success delete post!': post_pk })
 
+@api_view(['GET', 'POST'])
+def comment_list_create(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'GET':
+        comments = post.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            comment = serializer.save(post=post)
+            return Response(serializer.data)
+# 왜 post가 알아서 안들어가징??
+
+@api_view(['DELETE'])
+def comment_delete(request, post_pk, comment_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    comment = get_object_or_404(Comment, post=post, pk=comment_pk)
+    comment.delete()
+    return Response({ 'success delete comment!': comment_pk })
